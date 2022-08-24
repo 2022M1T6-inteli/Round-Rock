18db88b20175d122e314eeba6fe2d4ebea9d18aa
Vitorhrds2 <110406986+Vitorhrds2@users.noreply.github.com>
2022-08-24 18:41:34 -0300

    Protótipo-24-08

---
+++ Projeto6-main/ABC/menu_principal.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _on_play_button_pressed(): #this fuction is to start the game
+	get_tree().change_scene("res://Node2D.tscn")
+	
+func _on_config_button_pressed():#leva para o menu do jogo, onde é possível fazer ajustes
+	get_tree().change_scene("res://configuracoes.tscn")
+
+func _on_quit_button_pressed():#essa função fecha o jogo
+	get_tree().quit()
